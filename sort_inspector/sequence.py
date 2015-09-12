9107d0517ca28866fb92ebd5aa81b8a3e454844a
Josh Burroughs <josh@qhool.com>
2015-09-12 01:44:14 -0400

    integrate Comparable into Sequence
      * Sequence now wraps each value in a Comparable() object
      * add count_comparisons() and count_swaps() methods
      * to test the count_X() methods, compare these numbers to those
        returned by a simple manually instrumented bubblesort

--- sort_inspector/sequence.py
+++ sort_inspector/sequence.py
@@ -1,13 +1,14 @@
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 import math
+from .comparable import Comparable
 
 def slice_size(s):
     raw = (s.stop - s.start) / float(s.step or 1)
     return max(int(math.ceil(raw)),0)
 
 class Sequence(list):
-    def __init__(self, *args):
-        list.__init__(self, *args)
+    def __init__(self, t=[]):
+        list.__init__(self, [Comparable(x) for x in t])
         self.updates = 0
 
     def __setitem__(self, i, y):
@@ -22,3 +23,11 @@ class Sequence(list):
         self.updates += j - i
         list.__setslice__(self,i,j,y)
 
+    def count_comparisons(self):
+        total = 0
+        for x in self:
+            total += x.comparisons
+        return total
+
+    def count_swaps(self):
+        return self.updates / 2
--- test/test_sequence.py
+++ test/test_sequence.py
@@ -5,6 +5,22 @@ from sort_inspector.sequence import slice_size
 
 import random
 
+# bubble sort, manually instrumented
+# used to test swap/comparison counting
+def bubble(t):
+    swaps = 0
+    compares = 0
+    done = False
+    while not done:
+        done = True
+        for i in range(1,len(t)):
+            compares += 1
+            if t[i] < t[i-1]:
+                done = False
+                swaps += 1
+                t[i-1],t[i] = t[i],t[i-1]
+    return (swaps,compares)
+
 class SequenceTest(unittest.TestCase):
 
     def test_init(self):
@@ -30,7 +46,15 @@ class SequenceTest(unittest.TestCase):
                     for sgn in [-1,1]:
                         s = slice(i,j,sgn*st)
                         sz = slice_size(s)
-                        self.assertEqual(len(t[s]),sz) 
+                        self.assertEqual(len(t[s]),sz)
+
+    def test_counts(self):
+        for n in range(1,20):
+            for i in range(5):
+                t = Sequence([ random.randrange(20) for x in range(n) ])
+                (swaps,comps) = bubble(t)
+                self.assertEqual(t.count_comparisons(),comps)
+                self.assertEqual(t.count_swaps(),swaps)
 
 if __name__ == '__main__':
     unittest.main()
